1150bd60f309cbdaac7b5d2b1ef226edc11ab685
ralucoita <ralucaatamasan@yahoo.ro>
2022-12-17 16:11:32 +0200

    Add project files.

---
+++ Data/Tamasan_Raluca_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tamasan_Raluca_Lab2.Models;
+
+namespace Tamasan_Raluca_Lab2.Data
+{
+    public class Tamasan_Raluca_Lab2Context : DbContext
+    {
+        public Tamasan_Raluca_Lab2Context (DbContextOptions<Tamasan_Raluca_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tamasan_Raluca_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Tamasan_Raluca_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Tamasan_Raluca_Lab2.Models.Author> Author { get; set; }
+    }
+}
