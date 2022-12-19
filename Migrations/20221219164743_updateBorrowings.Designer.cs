e2424868a81f5f7dd815e4da4f7d7809c6f983d3
ralucoita <ralucaatamasan@yahoo.ro>
2022-12-19 19:01:02 +0200

    Lab 5 part 1

--- Migrations/20221219164743_updateBorrowings.Designer.cs
+++ Migrations/20221219164743_updateBorrowings.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tamasan_Raluca_Lab2.Data;
 
@@ -11,9 +12,10 @@ using Tamasan_Raluca_Lab2.Data;
 namespace Tamasan_Raluca_Lab2.Migrations
 {
     [DbContext(typeof(Tamasan_Raluca_Lab2Context))]
-    partial class Tamasan_Raluca_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221219164743_updateBorrowings")]
+    partial class updateBorrowings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
